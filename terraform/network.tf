9a21b2fefee38d81be121486614b10e0d5b1485d
sdragos1 <dragos.suceveanu@epitech.eu>
2024-11-28 14:15:18 +0100

    :sparkles: Add firewall ssh config

--- terraform/network.tf
+++ terraform/network.tf
@@ -9,3 +9,15 @@ resource "google_compute_subnetwork" "subnet" {
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
 }
+
+resource "google_compute_firewall" "vpc_firewall_ssh" {
+  name          = "vpc-firewall-ssh"
+  network       = google_compute_network.vpc.name
+  target_tags   = ["whanos-vm"]
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}

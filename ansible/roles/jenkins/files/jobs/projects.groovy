c75b7ed9a55d54ae69b22fb8e1ea6d2ed5c45ff2
sdragos1 <dragos.suceveanu@epitech.eu>
2024-12-06 19:26:28 +0100

    :sparkles: add project job creation

--- ansible/roles/jenkins/files/jobs/projects.groovy
+++ ansible/roles/jenkins/files/jobs/projects.groovy
@@ -1,10 +1,31 @@
 folder('Projects') {
-    description('Projects linked via the link-project job')
+    description('The folder for all the projects')
 }
 
 freeStyleJob('link-project') {
-    description('This job is used to link to another project')
+    description('This job is used to link a specified project to the Jenkins infrastructure ' +
+                'by creating a new job in the Projects folder.')
+
+    parameters {
+        stringParam('PROJECT_NAME', '', 'The name of the project to link to')
+    }
+
     steps {
-        shell('echo "This is a dummy job"')
+        dsl {
+            text('''
+                def projectName = "${PROJECT_NAME}".trim()
+
+                if (projectName) {
+                    freeStyleJob("Projects/${projectName}") {
+                        description("This job is used to link to the ${projectName} project")
+                        steps {
+                            shell('echo "This is a dummy job for ${projectName}"')
+                        }
+                    }
+                } else {
+                    error("No project name provided.")
+                }
+            '''.stripIndent())
+        }
     }
 }

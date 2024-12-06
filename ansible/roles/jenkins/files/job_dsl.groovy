379c57cb4b1a355696961698c8bcf46850dc732a
sdragos1 <dragos.suceveanu@epitech.eu>
2024-12-06 14:25:21 +0100

    :sparkles: setup jobs and folders

--- ansible/roles/jenkins/files/job_dsl.groovy
+++ ansible/roles/jenkins/files/job_dsl.groovy
@@ -1,10 +1,32 @@
 folder('Projects') {
-    description('Projects stored in the current instance')
+    description('Projects linked via the link-project job')
+}
+
+folder('Whanos base images') {
+    description('The base images for Whanos')
 }
 
 freeStyleJob('link-project') {
     description('This job is used to link to another project')
     steps {
         shell('echo "This is a dummy job"')
+    }
+}
+
+freeStyleJob('Whanos base images/Build all base images') {
+  description('This job is used to build all the base images')
+  steps {
+    shell('echo "This is a dummy job"')
+  }
+}
+
+def whanosLanguages = ['c', 'python', 'java', 'javascript', 'befunge']
+
+whanosLanguages.each { language ->
+    freeStyleJob("Whanos base images/whanos-${language}") {
+        description("This job is used to build the ${language} base image")
+        steps {
+          shell('echo "This is a dummy job"')
+        }
     }
 }

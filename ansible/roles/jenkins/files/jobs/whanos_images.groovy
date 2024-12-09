9169289d0167f51a59bcf4d1ca403222cf763e95
BxptisteM <baptiste.moreau@epitech.eu>
2024-12-09 17:02:00 +0100

    :sparkles: Added docker installation on the master vm

--- ansible/roles/jenkins/files/jobs/whanos_images.groovy
+++ ansible/roles/jenkins/files/jobs/whanos_images.groovy
@@ -1,21 +1,31 @@
+def whanosLanguages = ['c', 'python', 'java', 'javascript', 'befunge']
+
 folder('Whanos base images') {
     description('The base images for Whanos')
 }
 
 freeStyleJob('Whanos base images/Build all base images') {
-    description('This job is used to build all the base images')
+    description('This job triggers the build of all base images.')
     steps {
-        shell('echo "This is a dummy job"')
+        shell("""
+            echo "Building all base images"
+            docker build -t whanos-c ./whanos-c
+            docker build -t whanos-python ./whanos-python
+            docker build -t whanos-java ./whanos-java
+            docker build -t whanos-javascript ./whanos-javascript
+            docker build -t whanos-befunge ./whanos-befunge
+        """)
     }
 }
 
-def whanosLanguages = ['c', 'python', 'java', 'javascript', 'befunge']
-
 whanosLanguages.each { language ->
     freeStyleJob("Whanos base images/whanos-${language}") {
-        description("This job is used to build the ${language} base image")
+        description("This job builds the ${language} base image.")
         steps {
-            shell('echo "This is a dummy job"')
+            shell("""
+                echo "Building ${language} base image"
+                docker build -t whanos-${language}: ./whanos-${language}
+            """)
         }
     }
 }

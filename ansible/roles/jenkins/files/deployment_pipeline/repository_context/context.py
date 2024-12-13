256c201b830ec765fe9e80b7fa7b8ce38a872830
sdragos1 <dragos.suceveanu@epitech.eu>
2024-12-13 14:27:12 +0100

    :art: fix lint errors

--- ansible/roles/jenkins/files/deployment_pipeline/repository_context/context.py
+++ ansible/roles/jenkins/files/deployment_pipeline/repository_context/context.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
 from repository_context.language_enum import Language
-from repository_context.detect import detect_language, detect_standalone, detect_deployable
-
+from repository_context.detect import (
+    detect_language,
+    detect_standalone,
+    detect_deployable,
+)
 
 
 @dataclass
